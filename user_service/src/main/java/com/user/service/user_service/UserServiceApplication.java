ac1f43ec720294dff4b76c2d876a887379407035
Pablo Abal Diez <112098403+pabloabaldiez@users.noreply.github.com>
2023-11-22 13:05:32 -0300

    servidor con git8

--- user_service/src/main/java/com/user/service/user_service/UserServiceApplication.java
+++ user_service/src/main/java/com/user/service/user_service/UserServiceApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
-
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
