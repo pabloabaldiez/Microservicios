9ed0a81a6dbcbe3a6c6602428ecf4c43bf86697e
Pablo Abal Diez <112098403+pabloabaldiez@users.noreply.github.com>
2023-11-23 12:29:46 -0300

    primer push

---
+++ bike_service/src/main/java/com/bike/service/repository/BikeRepository.java
@@ -0,0 +1,15 @@
+package com.bike.service.repository;
+
+
+import com.bike.service.entity.Bike;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BikeRepository extends JpaRepository<Bike, Integer> {
+
+    List<Bike> findByUserId(int userId);
+}
