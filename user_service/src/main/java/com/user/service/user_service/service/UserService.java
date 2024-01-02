bbb53c549008a4619c0147d0724507184410c2f6
Pablo Abal Diez <112098403+pabloabaldiez@users.noreply.github.com>
2024-01-02 11:49:00 -0300

    CB_Ok

--- user_service/src/main/java/com/user/service/user_service/service/UserService.java
+++ user_service/src/main/java/com/user/service/user_service/service/UserService.java
@@ -42,13 +42,13 @@ public class UserService {
     private RestTemplate restTemplate;
 
     public List<CarModel> getCar(int userId) {
-        List<CarModel> car = restTemplate.getForObject("http://localhost:8080/car/user/" + userId, List.class);
+        List<CarModel> car = restTemplate.getForObject("http://car-service/car/user/" + userId, List.class);
 
         return car;
     }
 
     public List<BikeModel> getBike(int userId) {
-        List<BikeModel> bike = restTemplate.getForObject("http://localhost:8080/bike/user/" + userId, List.class);
+        List<BikeModel> bike = restTemplate.getForObject("http://bike-service/bike/user/" + userId, List.class);
 
         return bike;
     }
